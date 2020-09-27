a8f62a62aeecdccaa394ab9d9d0bdfe02a6fe0ac
陳鎮宇 <ccking37@gmail.com>
2020-09-27 12:04:01 +0800

    Updates

---
+++ precache-manifest.60a39bdc4e2e3beb025f1125cd51ef7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce20171fdeb33a81bb0d84a0e6ef9ac",
+    "url": "/joke-teller-react/index.html"
+  },
+  {
+    "revision": "c67966a06bcb85d526b6",
+    "url": "/joke-teller-react/static/css/main.e231e738.chunk.css"
+  },
+  {
+    "revision": "98fff038686dfba5e6bc",
+    "url": "/joke-teller-react/static/js/2.4c8c76be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joke-teller-react/static/js/2.4c8c76be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67966a06bcb85d526b6",
+    "url": "/joke-teller-react/static/js/main.ed46476a.chunk.js"
+  },
+  {
+    "revision": "75dc30a7f51fb72336ef",
+    "url": "/joke-teller-react/static/js/runtime-main.a2ca8539.js"
+  },
+  {
+    "revision": "cf2a845542047ab9ad1a919ad0e7c74a",
+    "url": "/joke-teller-react/static/media/robot.cf2a8455.gif"
+  }
+]);
